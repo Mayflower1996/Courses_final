a021ca58524ee812318d85b8d7fbe8a28d2067e6
IvanOgurtsov1996 <jjgraffity@gmail.com>
2023-11-29 22:06:27 +0300

    Some pipline fixes

--- Tests/user_tests_and_data/user_tests/conftest.py
+++ Tests/user_tests_and_data/user_tests/conftest.py
@@ -1,8 +1,5 @@
 import pytest
-import requests
-
-from user_data.url_headers_auth_token import URL, HEADERS
-from user_data.user_create_payload import CORRECT_ONE_USER_PAYLOAD
+from user_data.url_headers_auth_token import URL
 
 
 @pytest.fixture(params=[f"{URL}/createWithArray", f"{URL}/createWithList"])
