ef507f6c3b9f6e16fb6b83425397366d155f8934
IvanOgurtsov1996 <jjgraffity@gmail.com>
2023-11-29 12:04:00 +0300

    Final version of 'test_user_createWithArray.py'

--- Tests/user_tests_and_data/user_data/user_createWithArray_payload.py
+++ Tests/user_tests_and_data/user_data/user_createWithArray_payload.py
@@ -1,5 +1,4 @@
-CORRECT_TWO_USERS_PAYLOAD = [
-        {
+CORRECT_TWO_USERS_PAYLOAD = [{
             "id": 1,
             "username": "testuser1",
             "firstName": "John",
@@ -18,10 +17,20 @@ CORRECT_TWO_USERS_PAYLOAD = [
             "password": "password2",
             "phone": "9876543210",
             "userStatus": 0
-        }
-    ]
+        }]
 
-CORRECT_USER_PAYLOAD_EMPTY_FIELDS = [{
+CORRECT_ONE_USER_PAYLOAD = [{
+            "id": 1,
+            "username": "testuser1",
+            "firstName": "John",
+            "lastName": "Doe",
+            "email": "john@example.com",
+            "password": "password1",
+            "phone": "1234567890",
+            "userStatus": 0
+        }]
+
+INCORRECT_USER_PAYLOAD_EMPTY_FIELDS = [{
         "id": 3,
         "username": "",
         "firstName": "",
@@ -32,11 +41,11 @@ CORRECT_USER_PAYLOAD_EMPTY_FIELDS = [{
         "userStatus": 0
     }]
 
-INCORRECT_USER_PAYLOAD_MISSING_FIELD = [{
+INCORRECT_USER_PAYLOAD_NOT_ARRAY = {
         "id": 0,
         "firstName": "Alice",
         "email": "john@example.com",
         "lastName": "Smith",
         "password": "password3",
         "phone": "5555555555",
-    }]
+    }
--- Tests/user_tests_and_data/user_tests/test_user_createWithArray.py
+++ Tests/user_tests_and_data/user_tests/test_user_createWithArray.py
@@ -1,9 +1,16 @@
 import requests
 from jsonschema import validate
-from user_data.user_createWithArray_payload import CORRECT_TWO_USERS_PAYLOAD, INCORRECT_USER_PAYLOAD_MISSING_FIELD, \
-    CORRECT_USER_PAYLOAD_EMPTY_FIELDS
+from user_data.user_createWithArray_payload import CORRECT_TWO_USERS_PAYLOAD, INCORRECT_USER_PAYLOAD_NOT_ARRAY, \
+    INCORRECT_USER_PAYLOAD_EMPTY_FIELDS, CORRECT_ONE_USER_PAYLOAD
 from user_data.url_headers_auth_token import HEADERS, URL
 from user_data.user_response_schema import RESPONSE_SCHEMA
+
+
+def test_response_schema():
+    response = requests.post(f"{URL}/createWithArray", headers=HEADERS, json=CORRECT_ONE_USER_PAYLOAD)
+    assert response.status_code == 200
+    assert "ok" in response.text.lower()
+    validate(instance=response.json(), schema=RESPONSE_SCHEMA)
 
 
 def test_create_two_users():
@@ -12,14 +19,13 @@ def test_create_two_users():
     assert "ok" in response.text.lower()
 
 
-def test_create_user_with_empty_fields_and_correct_schema():
-    response = requests.post(f"{URL}/createWithArray", headers=HEADERS, json=CORRECT_USER_PAYLOAD_EMPTY_FIELDS)
-    assert response.status_code == 200
-    assert "ok" in response.text
-    validate(instance=response.json(), schema=RESPONSE_SCHEMA)
+def test_create_user_not_array():
+    response = requests.post(f"{URL}/createWithArray", headers=HEADERS, json=INCORRECT_USER_PAYLOAD_NOT_ARRAY)
+    assert response.status_code == 500
+    assert "something bad happened" in response.text
 
 
-def test_create_user_with_missing_field():
-    response = requests.post(f"{URL}/createWithArray", headers=HEADERS, json=INCORRECT_USER_PAYLOAD_MISSING_FIELD)
+def test_create_user_with_empty_fields():
+    response = requests.post(f"{URL}/createWithArray", headers=HEADERS, json=INCORRECT_USER_PAYLOAD_EMPTY_FIELDS)
     assert response.status_code == 500
     assert "something bad happened" in response.text
